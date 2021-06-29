43a7e589e278b332b8ab02ffb2d022018ab171d5
Angel <53478143+007sya@users.noreply.github.com>
2021-06-29 14:47:21 +0200

    Working on the Password Creation form view

--- src/views/PasswordCreationForm/PasswordCreationForm.jsx
+++ src/views/PasswordCreationForm/PasswordCreationForm.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import PasswordValidationInput from "components/PasswordValidationInput/PasswordValidationInput";
 
+import infoIcon from "assets/icons/info.svg";
 import "./PasswordCreationForm.scss";
+
+const CLUE_MAXIMUM_LENGTH = 255;
 
 export default function PasswordCreationForm() {
   const [firstPassword, setFirstPassword] = useState("");
   const [secondPassword, setSecondPassword] = useState("");
+  const [showExtraInfo, setShowExtraInfo] = useState(false);
+
+  const [clue, setClue] = useState("");
   return (
     <div className="PasswordCreationForm">
       <div>
@@ -19,18 +25,52 @@ export default function PasswordCreationForm() {
           <PasswordValidationInput
             value={firstPassword}
             onChange={(e) => setFirstPassword(e.target.value)}
-            placeHolder="Introduce tu contraseña"
+            placeholder="Introduce tu contraseña"
           />
         </div>
         <div className="column">
           <strong>Repite tu Contraseña Maestra</strong>
           <PasswordValidationInput
             value={secondPassword}
-            setValue={setSecondPassword}
             onChange={(e) => setSecondPassword(e.target.value)}
-            placeHolder="Repite tu contraseña"
+            placeholder="Repite tu contraseña"
           />
         </div>
+      </div>
+      <div>
+        También, puedes crear una pista que te ayuda a recordar tu contraseña
+        maestra.
+      </div>
+      <div className="column">
+        <strong className="row vertical-centered">
+          Crea tu pista para recordar tu contraseña (opcional)
+          <img
+            className="info-icon"
+            src={infoIcon}
+            onClick={() => setShowExtraInfo(true)}
+          ></img>
+          {showExtraInfo ? (
+            <div className="extra-info">
+              Dar una pista para que puedas recordar la contraseña es muy
+              recomendable ya que en el caso de que pierdas no podemos hacer
+              nada para ayudarte a recuperarla
+              <button
+                className="hide-extra-info secondary"
+                onClick={() => setShowExtraInfo(false)}
+              >
+                X
+              </button>
+            </div>
+          ) : null}
+        </strong>
+        <input
+          placeholder="Introduce tu pista"
+          value={clue}
+          onChange={(e) => {
+            if (e.target.value.length < 256) setClue(e.target.value);
+          }}
+        ></input>
+        <div className="clue-length-indicator">{`${clue.length}/${CLUE_MAXIMUM_LENGTH}`}</div>
       </div>
     </div>
   );
