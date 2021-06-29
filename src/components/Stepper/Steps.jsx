afdcd74582f547ed44d7d7c52c329617e7caa9c5
Angel <53478143+007sya@users.noreply.github.com>
2021-06-29 19:55:43 +0200

    Finished Stepper component

--- src/components/Stepper/Steps.jsx
+++ src/components/Stepper/Steps.jsx
@@ -1,15 +1,23 @@
 import React, { Fragment } from "react";
 
+function getClassName(index, activeStepIndex) {
+  if (activeStepIndex > index) {
+    return (
+      "step-indicator" +
+      (activeStepIndex === index ? "active passed" : " passed")
+    );
+  }
+  return "step-indicator" + (activeStepIndex === index ? "-active" : "");
+}
+
 export default function Steps(props) {
   return props.steps.map((step, index) => (
     <Fragment key={index}>
       <div
         ref={props.indicatorRefs[index]}
-        className={
-          "step-indicator" + (props.activeStepIndex === index ? "-active" : "")
-        }
+        className={getClassName(index, props.activeStepIndex)}
       >
-        {step}
+        {props.activeStepIndex > index ? "✓" : step}
       </div>
       {index + 1 !== props.steps.length ? (
         <div className="step-separator"></div>
